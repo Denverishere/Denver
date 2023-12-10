f1251b1149b4ab3d66839c403b0625872970425d
Denverishere <132921421+Denverishere@users.noreply.github.com>
2023-12-10 22:56:41 +0530

    Add files via upload

---
+++ addon.py
@@ -0,0 +1,2 @@
+from resources.lib import main
+if __name__=='__main__':main.run()
---
+++ service.py
@@ -0,0 +1,20 @@
+from resources.lib import proxy
+from codequick import Script
+from codequick.script import Settings
+from socketserver import ThreadingTCPServer
+import threading
+from xbmc import Monitor,executebuiltin
+from kodi_six import xbmcgui
+def serveForever(handler):
+	try:handler.serve_forever()
+	except Exception as A:Script.log(A,lvl=Script.DEBUG)
+ThreadingTCPServer.allow_reuse_address=True
+_PORT=48996
+handler=ThreadingTCPServer(('',_PORT),proxy.JioTVProxy)
+t=threading.Thread(target=serveForever,args=(handler,))
+t.setDaemon(True)
+t.start()
+if Settings.get_boolean('m3ugen'):executebuiltin('RunPlugin(plugin://plugin.video.jiotv/resources/lib/main/m3ugen/?notify=no)')
+monitor=Monitor()
+while not monitor.abortRequested():
+	if monitor.waitForAbort(10):handler.shutdown();handler.server_close();break
